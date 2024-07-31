1035272e1fb4485d846c1770c9c26387ee38dfca
alinanjum-99 <alinanjum1999@gmail.com>
2024-07-31 18:26:15 -0500

    Initial commit

---
+++ models/Doctor.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const doctorSchema = new Schema({
+    name: { type: String, required: true },
+    specialty: { type: String, required: true },
+    // Add more fields as needed
+});
+ 
+const Doctor =
+    mongoose.model('Doctor', doctorSchema);
+ 
+module.exports = Doctor;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const patientsRouter = require('./routes/patients');
+const doctorsRouter = require('./routes/doctors');
+const appoinmentsRouter = require('./routes/appointments')
+const app = express();
+const PORT = process.env.PORT || 5000;
+ 
+app.use(cors());
+app.use(bodyParser.json());
+ 
+// Connect to MongoDB
+mongoose.connect(
+    'mongodb+srv://alinanjum1999:Alinanjum99!@cluster0.l3sztjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    );
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+ 
+app.use('/patients', patientsRouter);
+app.use('/doctors', doctorsRouter);
+app.use('/appointments', appoinmentsRouter)
+ 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
